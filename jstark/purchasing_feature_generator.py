ed5da373462625117e8946438692686b3c81a361
Jamie Thomson <jamie@jamie-thomson.net>
2022-11-13 22:12:54 +0000

    rename go to get_df

--- jstark/purchasing_feature_generator.py
+++ jstark/purchasing_feature_generator.py
@@ -5,5 +5,5 @@ class PurchasingFeatureGenerator(object):
     def __init__(self, df: DataFrame) -> None:
         self.__df = df
 
-    def go(self):
+    def get_df(self):
         return self.__df
--- tests/test_purchasing_feature_generator.py
+++ tests/test_purchasing_feature_generator.py
@@ -4,5 +4,5 @@ from jstark.purchasing_feature_generator import PurchasingFeatureGenerator
 
 
 def test_generator_returns_dataframe(dataframe_of_purchases):
-    ret = PurchasingFeatureGenerator(dataframe_of_purchases).go()
+    ret = PurchasingFeatureGenerator(dataframe_of_purchases).get_df()
     assert isinstance(ret, DataFrame)
