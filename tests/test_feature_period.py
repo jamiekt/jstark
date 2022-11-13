b69b031616735cce5da593ae25f72cc4df65c9d8
Jamie Thomson <jamie@jamie-thomson.net>
2022-11-13 12:35:17 +0000

    error message changes in python3.11

--- tests/test_feature_period.py
+++ tests/test_feature_period.py
@@ -1,3 +1,6 @@
+from platform import python_version
+from packaging import version
+
 import pytest
 
 from jstark.feature import FeaturePeriod, PeriodUnitOfMeasure
@@ -13,19 +16,28 @@ def test_feature_period_start_is_immutable():
     feature_period = FeaturePeriod(PeriodUnitOfMeasure.DAY, 3, 2)
     with pytest.raises(AttributeError) as excInfo:
         feature_period.start = 10
-    assert  "can't set attribute" in  str(excInfo.value)
+    if version.parse(python_version()) < version.parse("3.11"):
+        assert  "can't set attribute" in  str(excInfo.value)
+    else:
+        assert str(excInfo.value) == "property 'start' of 'FeaturePeriod' object has no setter"
 
 def test_feature_period_end_is_immutable():
     feature_period = FeaturePeriod(PeriodUnitOfMeasure.DAY, 3, 2)
     with pytest.raises(AttributeError) as excInfo:
         feature_period.end = 10
-    assert  "can't set attribute" in  str(excInfo.value)
+    if version.parse(python_version()) < version.parse("3.11"):
+        assert  "can't set attribute" in  str(excInfo.value)
+    else:
+        assert str(excInfo.value) == "property 'end' of 'FeaturePeriod' object has no setter"
 
 def test_feature_period_periodunitofmeasure_is_immutable():
     feature_period = FeaturePeriod(PeriodUnitOfMeasure.DAY, 3, 2)
     with pytest.raises(AttributeError) as excInfo:
         feature_period.period_unit_of_measure = PeriodUnitOfMeasure.MONTH
-    assert  "can't set attribute" in  str(excInfo.value)
+    if version.parse(python_version()) < version.parse("3.11"):
+        assert  "can't set attribute" in  str(excInfo.value)
+    else:
+        assert str(excInfo.value) == "property 'period_unit_of_measure' of 'FeaturePeriod' object has no setter"
 
 def test_feature_period_end_greater_than_start_raises_exception():
     with pytest.raises(FeaturePeriodEndGreaterThanStartError) as excInfo:
