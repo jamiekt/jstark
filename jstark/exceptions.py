501d19bf8b30c9a71838a1d1f69058e448ede597
Jamie Thomson <jamie@jamie-thomson.net>
2022-11-13 16:36:24 +0000

    placate black linter

--- jstark/exceptions.py
+++ jstark/exceptions.py
@@ -1,12 +1,10 @@
 class FeaturePeriodEndGreaterThanStartError(Exception):
-    """Exception indicating end of a feature period cannot be before the start
+    """Exception indicating end of a feature period cannot be before the start"""
 
-    """
-    
     def __init__(self, start: int, end: int, *args: object) -> None:
         super().__init__(*args)
         self.start = start
         self.end = end
-    
+
     def __str__(self) -> str:
         return f"End of the feature period ({self.end}) cannot be before the start of the feature period ({self.start})"
--- tests/test_feature_period.py
+++ tests/test_feature_period.py
@@ -6,40 +6,57 @@ import pytest
 from jstark.feature import FeaturePeriod, PeriodUnitOfMeasure
 from jstark.exceptions import FeaturePeriodEndGreaterThanStartError
 
+
 def test_feature_period_description():
     assert (
         FeaturePeriod(PeriodUnitOfMeasure.DAY, 3, 2).description
         == "Between 3 and 2 days ago"
     )
 
+
 def test_feature_period_start_is_immutable():
     feature_period = FeaturePeriod(PeriodUnitOfMeasure.DAY, 3, 2)
     with pytest.raises(AttributeError) as excInfo:
         feature_period.start = 10
     if version.parse(python_version()) < version.parse("3.11"):
-        assert  "can't set attribute" in  str(excInfo.value)
+        assert "can't set attribute" in str(excInfo.value)
     else:
-        assert str(excInfo.value) == "property 'start' of 'FeaturePeriod' object has no setter"
+        assert (
+            str(excInfo.value)
+            == "property 'start' of 'FeaturePeriod' object has no setter"
+        )
+
 
 def test_feature_period_end_is_immutable():
     feature_period = FeaturePeriod(PeriodUnitOfMeasure.DAY, 3, 2)
     with pytest.raises(AttributeError) as excInfo:
         feature_period.end = 10
     if version.parse(python_version()) < version.parse("3.11"):
-        assert  "can't set attribute" in  str(excInfo.value)
+        assert "can't set attribute" in str(excInfo.value)
     else:
-        assert str(excInfo.value) == "property 'end' of 'FeaturePeriod' object has no setter"
+        assert (
+            str(excInfo.value)
+            == "property 'end' of 'FeaturePeriod' object has no setter"
+        )
+
 
 def test_feature_period_periodunitofmeasure_is_immutable():
     feature_period = FeaturePeriod(PeriodUnitOfMeasure.DAY, 3, 2)
     with pytest.raises(AttributeError) as excInfo:
         feature_period.period_unit_of_measure = PeriodUnitOfMeasure.MONTH
     if version.parse(python_version()) < version.parse("3.11"):
-        assert  "can't set attribute" in  str(excInfo.value)
+        assert "can't set attribute" in str(excInfo.value)
     else:
-        assert str(excInfo.value) == "property 'period_unit_of_measure' of 'FeaturePeriod' object has no setter"
+        assert (
+            str(excInfo.value)
+            == "property 'period_unit_of_measure' of 'FeaturePeriod' object has no setter"
+        )
+
 
 def test_feature_period_end_greater_than_start_raises_exception():
     with pytest.raises(FeaturePeriodEndGreaterThanStartError) as excInfo:
         FeaturePeriod(PeriodUnitOfMeasure.DAY, 2, 3)
-    assert str(excInfo.value) == "End of the feature period (3) cannot be before the start of the feature period (2)"
+    assert (
+        str(excInfo.value)
+        == "End of the feature period (3) cannot be before the start of the feature period (2)"
+    )
