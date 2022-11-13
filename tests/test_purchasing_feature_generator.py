1604ce9e7bd939a371b27cb32046e70440e8c003
Jamie Thomson <jamie@jamie-thomson.net>
2022-11-13 22:44:01 +0000

    black linted

--- tests/test_purchasing_feature_generator.py
+++ tests/test_purchasing_feature_generator.py
@@ -5,5 +5,7 @@ from jstark.purchasing_feature_generator import PurchasingFeatureGenerator
 
 
 def test_generator_returns_dataframe(dataframe_of_purchases):
-    df_out = PurchasingFeatureGenerator(as_at=date(2023,11,13), df=dataframe_of_purchases).get_df()
+    df_out = PurchasingFeatureGenerator(
+        as_at=date(2023, 11, 13), df=dataframe_of_purchases
+    ).get_df()
     assert isinstance(df_out, DataFrame)
