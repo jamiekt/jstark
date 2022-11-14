b4ee8a7ea36b7b48a66db2862d2ed04ddb5930ff
Jamie Thomson <jamie@jamie-thomson.net>
2022-11-14 21:55:22 +0000

    internal column to hold date of the transaction

--- jstark/purchasing_feature_generator.py
+++ jstark/purchasing_feature_generator.py
@@ -1,6 +1,7 @@
 from pyspark.sql import DataFrame
 from pyspark.sql.types import TimestampType
 from datetime import date
+import pyspark.sql.functions as f
 
 from jstark.gross_spend_feature import GrossSpendFeature
 from jstark.feature import FeaturePeriod, PeriodUnitOfMeasure
@@ -16,6 +17,8 @@ class PurchasingFeatureGenerator(object):
             or df.schema["Timestamp"].dataType is not TimestampType()
         ):
             raise DataFrameDoesNotIncludeTimestampColumn()
+        # Need a column containing the date of the transaction.
+        df = df.withColumn("~date~", f.to_date("Timestamp"))
 
     def get_df(self):
         gross_spend_feature = GrossSpendFeature(
