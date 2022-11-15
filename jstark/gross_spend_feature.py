7672c2cd59fd69ae8eebe8e585fded55788774b0
Jamie Thomson <jamie@jamie-thomson.net>
2022-11-15 22:06:28 +0000

    placate flake8 linter

--- jstark/gross_spend_feature.py
+++ jstark/gross_spend_feature.py
@@ -1,9 +1,9 @@
-from .feature import Feature
 from datetime import date
-from .feature import FeaturePeriod
-from pyspark.sql import DataFrame, Column
+
 import pyspark.sql.functions as f
-from jstark.feature import PeriodUnitOfMeasure
+from pyspark.sql import Column, DataFrame
+
+from .feature import Feature, FeaturePeriod
 
 
 class GrossSpendFeature(Feature):
@@ -11,6 +11,19 @@ class GrossSpendFeature(Feature):
         super().__init__(as_at, feature_period)
 
     def columnExpression(self, df: DataFrame) -> Column:
+        uom_abbreviation = (
+            "d"
+            if self.feature_period.period_unit_of_measure.name == "DAY"
+            else "w"
+            if self.feature_period.period_unit_of_measure.name == "WEEK"
+            else "m"
+            if self.feature_period.period_unit_of_measure.name == "MONTH"
+            else "q"
+            if self.feature_period.period_unit_of_measure.name == "QUARTER"
+            else "y"
+        )
         return f.sum(df["GrossSpend"]).alias(
-            f"GrossSpend_{self.feature_period.start}{'d' if self.feature_period.period_unit_of_measure.name == 'DAY' else 'w' if self.feature_period.period_unit_of_measure.name == 'WEEK' else 'm' if self.feature_period.period_unit_of_measure.name == 'MONTH' else 'q' if self.feature_period.period_unit_of_measure.name == 'QUARTER' else 'y'}{self.feature_period.end}"
+            f"GrossSpend_{self.feature_period.start}"
+            + f"{uom_abbreviation}"
+            + f"{self.feature_period.end}"
         )
