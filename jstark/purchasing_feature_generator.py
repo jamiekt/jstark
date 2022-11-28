1292dd9fe118c9cd10b1fa77f415bd2dc35ec22b
Jamie Thomson <jamie.thomson@moneysupermarket.com>
2022-11-28 13:45:49 +0000

    return a list of expressions instead of a dataframe
    This is a big change in direction. Previously it accepted a
    dataframe and passed back a dataframe after doing the aggregation
    within PurchasingFeatureGenerator. I don't like this approach
    because it assumes how the end user wants to perform the aggregation.
    They may want to use groupBy, they may want to use rollup or cube,
    or grouping sets. They will also want to choose which columns
    they group by.  I don't want to take those choices away from them.
    
    Hence, we're going to return a list of column expressions that
    the end user can pass to pyspark.sql.DataFrame.agg.

--- jstark/purchasing_feature_generator.py
+++ jstark/purchasing_feature_generator.py
@@ -1,57 +1,19 @@
 from datetime import date
 from typing import List
 
-import pyspark.sql.functions as f
-from pyspark.sql import DataFrame
-from pyspark.sql.types import TimestampType
-
-from jstark.exceptions import DataFrameDoesNotIncludeTimestampColumn
+from pyspark.sql import Column
 from jstark.feature_period import FeaturePeriod, PeriodUnitOfMeasure
 from jstark.gross_spend_feature import GrossSpend
 
 
 class PurchasingFeatureGenerator(object):
-    def __init__(
-        self,
-        as_at: date,
-        df: DataFrame,
-        customer_attr: str = "All",
-        product_attr: str = "All",
-        store_attr: str = "All",
-        channel_attr: str = "All",
-    ) -> None:
-        if (
-            "Timestamp" not in df.schema.fieldNames()
-            or df.schema["Timestamp"].dataType is not TimestampType()
-        ):
-            raise DataFrameDoesNotIncludeTimestampColumn()
-        # Need a column containing the date of the transaction.
-        df = df.withColumn("~date~", f.to_date("Timestamp"))
+    def __init__(self, as_at: date) -> None:
         self.__as_at = as_at
-        self.__df = df.withColumn("asAt", f.lit(self.__as_at))
-        self.__customer_attr = customer_attr
-        self.__product_attr = product_attr
-        self.__store_attr = store_attr
-        self.__channel_attr = channel_attr
 
-    @property
-    def grain(self) -> List[str]:
-        grain = []
-        if self.__customer_attr != "All":
-            grain.append(self.__customer_attr)
-        if self.__product_attr != "All":
-            grain.append(self.__product_attr)
-        if self.__store_attr != "All":
-            grain.append(self.__store_attr)
-        if self.__channel_attr != "All":
-            grain.append(self.__channel_attr)
-        return grain
-
-    def get_df(self):
+    def get_features(self) -> List[Column]:
         gross_spend = GrossSpend(
             as_at=self.__as_at,
             feature_period=FeaturePeriod(PeriodUnitOfMeasure.DAY, 2, 1),
         )
         expressions = [gross_spend.column]
-        output_df = self.__df.groupBy(self.grain).agg(*expressions)
-        return output_df
+        return expressions
