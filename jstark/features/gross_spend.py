79419e3b734d44bd3d5f43191abbf269352633ef
Jamie Thomson <jamie.thomson@moneysupermarket.com>
2022-12-09 21:40:55 +0000

    refactor - Sum abstract class

--- jstark/features/gross_spend.py
+++ jstark/features/gross_spend.py
@@ -2,23 +2,17 @@ from datetime import date
 
 import pyspark.sql.functions as f
 from pyspark.sql import Column
-from typing import Callable
 
-from .feature import Feature, FeaturePeriod
+from .feature import FeaturePeriod
+from .sum_feature import Sum
 
 
-class GrossSpend(Feature):
+class GrossSpend(Sum):
     def __init__(self, as_at: date, feature_period: FeaturePeriod) -> None:
         super().__init__(as_at, feature_period)
 
-    def aggregator(self) -> Callable[[Column], Column]:
-        return self.sum_aggregator
-
     def column_expression(self) -> Column:
         return f.col("GrossSpend")
-
-    def default_value(self) -> Column:
-        return f.lit(0)
 
     @property
     def description_subject(self) -> str:
