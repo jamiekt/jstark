b1442e60174b4461155fcadfb9361fe5c0c58a9e
Jamie Thomson <854953+jamiekt@users.noreply.github.com>
2026-04-24 15:19:30 +0100

    Fix failing test

--- tests/test_mealkit_features.py
+++ tests/test_mealkit_features.py
@@ -22,7 +22,7 @@ def test_orderweeks(
     )
     first = output_df.first()
     assert first is not None
-    assert first["OrderCount_52w0"] == 903
+    assert first["OrderCount_52w0"] == 845
 
 
 def test_allergens(dataframe_of_faker_mealkit_orders: DataFrame):
